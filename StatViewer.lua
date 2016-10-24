5b4ac824748ac123b7f47a5d24cf338b6ad96deb
Valentin <val_jub@live.fr>
2016-10-24 10:49:15 +0200

    first test with only 1 stat

--- StatViewer.lua
+++ StatViewer.lua
@@ -1,16 +1,22 @@
 StatViewer = {}
 
 StatViewer.name="StatViewer"
-
+StatViewer.maxHealth=""
 
 function StatViewer:Initiliaze()
-
+  EVENT_MANAGER:RegisterForEvent(StatViewer.name, EVENT_STATS_UPDATED, StatViewer.UpdateStats)
+  StatViewer.UpdateStats(nil)
 end
 
 function StatViewer.OnAddOnLoaded(event, addonName)
   if addonName==StatViewer.name then
     StatViewer.Initiliaze()
   end
+end
+
+function StatViewer.UpdateStats(event)
+  self.maxHealth=GetPlayerStat(STAT_HEALTH_MAX,STAT_BONUS_OPTION_APPLY_BONUS)
+  SVIndicator.Label:SetText(self.maxHealth)
 end
 
 EVENT_MANAGER:RegisterForEvent(StatViewer.name, EVENT_ADD_ON_LOADED, StatViewer.OnAddOnLoaded)
